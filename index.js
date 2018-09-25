0dcea6fea7ff77414c1cc74036462ff909daa246
Ezra T Huffman <joshooha@gmail.com>
2018-09-25 15:45:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -135,9 +135,9 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval);
-  for (var i = 0; i < ROCKS.length; i++) {
-    ROCKS[i].remove();
-  }
+  ROCKS.remove();
+  moveDodger.remove();
+  alert("YOU LOSE!");
   
 }
 function moveDodger(e) {
